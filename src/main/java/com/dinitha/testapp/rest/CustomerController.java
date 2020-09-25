36ff61b363f556d573a287f987936d08fb8c7d31
jalithakanchna <jalithakanchana@gmail.com>
2020-09-25 14:20:28 +0530

    first commit

--- src/main/java/com/dinitha/testapp/rest/CustomerController.java
+++ src/main/java/com/dinitha/testapp/rest/CustomerController.java
@@ -2,12 +2,14 @@ package com.dinitha.testapp.rest;
 
 import com.dinitha.testapp.entity.Customer;
 import com.dinitha.testapp.service.CustomerService;
+import com.dinitha.testapp.utilities.SemanticGAPI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,11 +22,24 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+    @Autowired
+    private SemanticGAPI semanticGAPI;
+
     @GetMapping("/list")
     public ResponseEntity<List<Customer>> getCustomerList(){
         log.info("HTTP GET --> retrieveFunderTypes -->  /firebase/url");
         return ResponseEntity.ok(customerService.listCustomers());
     }
+
+    @RequestMapping(value = "/semantic/analysis", method = RequestMethod.POST)
+    public ResponseEntity<List<Customer>> getAnalysisResponse() throws Exception {
+        log.info("HTTP GET --> retrieve FunderTypes -->  /firebase/url");
+        String[] args = {"sentiment","Michelangelo Caravaggio, Italian painter, is known for 'The Calling of Saint Matthew"};
+        semanticGAPI.SemanticAnalysis(args);
+        return ResponseEntity.ok(customerService.listCustomers());
+
+    }
+
 
 
 }
